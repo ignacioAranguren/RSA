d1e8905acbd6ca7a013c89a85701436e698f1938
ignacioAranguren <56113565+ignacioAranguren@users.noreply.github.com>
2020-12-10 15:08:09 +0100

    Funciones iniciales

---
+++ RSA.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 14:32:17 2020
+
+@author: ignacio
+"""
+from sympy import randprime
+
+def rsa():
+    mensaje = input('Introduzca el mesnaje a cifrar:')
+# Algoritmo de Euclides Extendido
+def egcd(a, b):
+    x, X = 0, 1
+    y, Y = 1, 0
+    while (b != 0):
+        B = b
+        q = a // b
+        a, b = b, a % b
+        x, X = X - q * x, x
+        y, Y = Y - q * y, y
+    return ([B, X, Y])
+
+# inverso modular
+def modInverse(a,m):
+    L = egcd(a,m)
+    return L[1] % m
+
+# exponenciación modular y 
+def mpow(x, y, z):
+    e = 1
+    while y:
+        if y & 1:
+            e = e * x % z
+        y >>= 1
+        x = x * x % z
+    return e
+
+#Suma todos los valores ascii de los caracteres del mensaje a base 10
+def sumMensaje(m):
+    L=list(map(ord, list(m)))
+    l = len(L)
+    L.reverse()
+    M=0
+    for i in range(l):
+        M+=L[i]*(256**i)
+    return M
+
+r = randprime(2**399, 2**400)
